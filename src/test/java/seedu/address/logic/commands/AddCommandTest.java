2bf86ebaf8a2105980a0aacc06e54e8c61f3f412
zhonggruii <leezhongrui1@gmail.com>
2025-10-21 20:47:54 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
@@ -324,6 +325,8 @@ public class AddCommandTest {
         @Override
         public boolean hasLesson(Lesson lesson) {
             throw new AssertionError("This method should not be called.");
+        }
+
         public void unarchivePerson(Person target) {
             throw new AssertionError("This method should not be called.");
         }
